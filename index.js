d14ef662af4df42d641f88811f73bc091f2861db
webApp <webapp@webApp-no-MacBook-Air.local>
2018-12-15 11:29:41 +0900

    hoge

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 "use strict";
 const express = require('express')
 const app = express()
-let WebSocketServer = require('ws').Server;
-let port = 3001;
-let wsServer = new WebSocketServer({ port: port });
-console.log('websocket server start. port=' + port);
- 
-wsServer.on('connection', function(ws) {
-  console.log('-- websocket connected --');
-  ws.on('message', function(message) {
-    wsServer.clients.forEach(function each(client) {
-      if (isSame(ws, client)) {
-        console.log('- skip sender -');
-      }
-      else {
-        client.send(message);
-      }
-    });
-  });
-});
 app.use('/', express.static('pages'));
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port hoge!'))
- 
-function isSame(ws1, ws2) {
-  // -- compare object --
-  return (ws1 === ws2);     
-}
